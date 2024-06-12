3a883ca15c062c6931ba0860fa0c593db74daa26
Santycm <santicmoreno@gmail.com>
2024-06-12 17:24:32 -0500

    add footer and modify nav colors

--- js/nav.js
+++ js/nav.js
@@ -7,32 +7,44 @@ const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu-btn");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
 
+const nav = document.querySelectorAll("#elementsNav > li");
+const bgNav = document.querySelector(".header");
+
+//Cambio de colores del nav al hacer scroll
+window.addEventListener("scroll", () => {
+  const scroll = document.documentElement.scrollTop;
+  if (window.innerWidth > 1200) {
+    if (scroll > 300) {
+      changeNavColors();
+    } else if (scroll < 300) {
+      resetNavColors();
+    }
+  } else if (window.innerWidth < 1200) {
+    changeNavColors();
+  }
+});
+
 [openMenuBtn, closeMenuBtn].forEach((btn) => {
   btn.addEventListener("click", () => {
     menu.classList.toggle("open");
-    if (window.innerWidth < 1200) {
-      showCloseMenuMobile();
+    changeNavColors();
+
+    if (!menu.classList.contains(menuAnimations.show)) {
+      menu.classList.add(menuAnimations.show);
+      menu.classList.remove(menuAnimations.hide);
+    } else {
+      menu.classList.add(menuAnimations.hide);
+      menu.classList.remove(menuAnimations.show);
     }
+    window.addEventListener("resize", () => {
+      if (window.innerWidth >= 1200) {
+        if (menu.classList.contains(menuAnimations.hide)) {
+          menu.classList.remove(menuAnimations.hide);
+        }
+      }
+    });
     menu.style.transition = "transform 05.s ease";
   });
-});
-
-function showCloseMenuMobile() {
-  if (!menu.classList.contains(menuAnimations.show)) {
-    menu.classList.add(menuAnimations.show);
-    menu.classList.remove(menuAnimations.hide);
-  } else {
-    menu.classList.add(menuAnimations.hide);
-    menu.classList.remove(menuAnimations.show);
-  }
-}
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth >= 1200) {
-    if (menu.classList.contains(menuAnimations.hide)) {
-      menu.classList.remove(menuAnimations.hide);
-    }
-  }
 });
 
 menu.addEventListener("transitionend", (e) => {
@@ -44,3 +56,33 @@ menu.querySelectorAll(".dropdown > i").forEach((arrow) => {
     e.target.closest(".dropdown").classList.toggle("active");
   });
 });
+
+function changeNavColors() {
+  nav.forEach(function (navElement) {
+    if (navElement.classList.contains("home")) {
+      return;
+    } else {
+      navElement.classList.add("bg-transparent");
+      navElement.classList.add("text-black");
+      navElement.classList.remove("bg-mainColor");
+      navElement.classList.remove("text-white");
+    }
+  });
+  bgNav.classList.add("bg-[#f2f2f2]");
+  bgNav.classList.remove("bg-transparent");
+}
+
+function resetNavColors() {
+  nav.forEach(function (navElement) {
+    if (navElement.classList.contains("home")) {
+      return;
+    } else {
+      navElement.classList.remove("bg-transparent");
+      navElement.classList.add("bg-mainColor");
+      navElement.classList.remove("text-black");
+      navElement.classList.add("text-white");
+    }
+  });
+  bgNav.classList.remove("bg-[#f2f2f2]");
+  bgNav.classList.add("bg-transparent");
+}
--- js/tailwindConfig.js
+++ js/tailwindConfig.js
@@ -5,6 +5,7 @@ tailwind.config = {
       colors: {
         gobColor: "#3366cc",
         navBg: "#f2f2f2",
+        homeNavBg: "#505152",
         mainColor: "#3c63ad",
         mainColorHover: "#074984",
         mainServiceBg: "#3a494f",
@@ -14,9 +15,9 @@ tailwind.config = {
         secondaryBlue: "#00b6ed",
         thirdBlue: "#3767af",
 
-        firstTitle:"#3c63ad",
+        firstTitle: "#3c63ad",
         secondTitle: "#5a5a58",
       },
     },
-  }
+  },
 };

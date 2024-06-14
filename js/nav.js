20614570ba94c6bccf756fb1882a8e2f391e7970
Santycm <santicmoreno@gmail.com>
2024-06-14 18:00:23 -0500

    fix carousel and navbar

--- js/nav.js
+++ js/nav.js
@@ -7,9 +7,20 @@ const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu-btn");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
 
-const nav = document.querySelectorAll("#elementsNav > li");
+const nav = document.querySelectorAll("#elementsNav > li ");
+const navDownIcon = document.querySelectorAll("#elementsNav > li > section > i");
+const homeNavBg = document.querySelector("#elementsNav > .home");
+const homeNavIcon = document.querySelector(
+  "#elementsNav > .home > a > div > svg"
+);
 const bgNav = document.querySelector(".header");
 
+//Vericar la posoción de scroll para aplicar estilos al Nav al momento de recargar la pagina
+if (document.documentElement.scrollTop > 200) {
+  changeNavColors();
+} else if (document.documentElement.scrollTop < 200) {
+  resetNavColors();
+}
 //Cambio de colores del nav al hacer scroll
 window.addEventListener("scroll", () => {
   const scroll = document.documentElement.scrollTop;
@@ -58,31 +69,41 @@ menu.querySelectorAll(".dropdown > i").forEach((arrow) => {
 });
 
 function changeNavColors() {
+  navDownIcon.forEach(function (icon) {
+    icon.classList.add("invert");
+  });
   nav.forEach(function (navElement) {
     if (navElement.classList.contains("home")) {
       return;
     } else {
       navElement.classList.add("bg-transparent");
       navElement.classList.add("text-black");
-      navElement.classList.remove("bg-mainColor");
+      navElement.classList.remove("bg-bgColor1");
       navElement.classList.remove("text-white");
     }
   });
   bgNav.classList.add("bg-[#f2f2f2]");
   bgNav.classList.remove("bg-transparent");
+  homeNavIcon.classList.add("fill-black");
+  homeNavBg.classList.add("bg-transparent");
 }
 
 function resetNavColors() {
+  navDownIcon.forEach(function (icon) {
+    icon.classList.remove("invert");
+  });
   nav.forEach(function (navElement) {
     if (navElement.classList.contains("home")) {
       return;
     } else {
       navElement.classList.remove("bg-transparent");
-      navElement.classList.add("bg-mainColor");
+      navElement.classList.add("bg-bgColor1");
       navElement.classList.remove("text-black");
       navElement.classList.add("text-white");
     }
   });
   bgNav.classList.remove("bg-[#f2f2f2]");
   bgNav.classList.add("bg-transparent");
+  homeNavIcon.classList.remove("fill-black");
+  homeNavBg.classList.remove("bg-transparent");
 }
--- js/tailwindConfig.js
+++ js/tailwindConfig.js
@@ -5,26 +5,16 @@ tailwind.config = {
       colors: {
         gobColor: "#3366cc",
 
-        navBg: "#f2f2f2",
-
-        homeNavBg: "#505152",
+        //Text and Background
         mainColor: "#3c63ad",
-        mainColorHover: "#074984",
-        mainServiceBg: "#3a494f",
-        ServiceBg: "#336ab2",
-        ServiceHover: "#00b8ee",
+        secondColor: "#282F30", //SecondTextColor
+        thirdColor: "#3a494f", //BgServices
+        fourthColor: "#505152", //BgHomeNav
 
-        secondaryBlue: "#00b6ed",
-        thirdBlue: "#3767af",
-
-        firstTitle: "#3c63ad",
-        secondTitle: "#5a5a58",
-
-        bgColor1: "#3c63ad",
-        bgColor2: "#f2f2f2", //BgNav
-        bgColor3: "#505152", //BgHomeNav
-
+        //Hovers and use Bg
         hoverColor1: "#00b6ed",
+        hoverColor2: "#00b8ee", //Hover and Bg in NOTICIAS
+        hoverColor3: "#074984",
       },
     },
   },
